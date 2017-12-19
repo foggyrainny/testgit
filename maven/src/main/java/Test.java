424bde0800154b1ba3d3c988385dd0c413f5511f
foggyrainny <1301428882@qq.com>
2017-12-19 11:14:08 +0800

    idea 第一次提交代码

---
+++ maven/src/main/java/Test.java
@@ -0,0 +1,15 @@
+public class Test {
+
+         public static void main(String[] args){
+
+              System.out.println("hello my java");
+
+              int s=0;
+              for(int i=1;i<=58; i++){
+                   s+=1;
+                   System.out.println(i);
+              }
+             System.out.println(s);
+           }
+
+}
